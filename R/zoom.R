6fc5c93502c4eac2ed135ebc44173f1f5ccfa490
zjcn86 <33260177+yikeshu0611@users.noreply.github.com>
2020-03-02 21:17:48 +0800

    Add files via upload

--- R/zoom.R
+++ R/zoom.R
@@ -18,11 +18,6 @@ zoom<-function(sf,ratio=0.5,center=c(0,0)){
         if (chinese()) stop(tmcn::toUTF8('zoom\u7684\u5BF9\u8C61\u5FC5\u987B\u662Fsf'))
         if (!chinese()) stop('object of zoom must be sf')
     }
-    if (length(ratio) != 1 |
-        length(ratio) != nrow(sf)){
-        if (chinese()) stop(tmcn::toUTF8('ratio\u5FC5\u987B\u662F1\u4E2A\u6570\u5B57,\u59820.5;\u6216\u8005\u548Csf\u7684\u5217\u6570\u76F8\u540C'))
-        if (!chinese()) stop('length of ratio must be 1 or equal to nrow of sf')
-    }
     if (is.character(center)){
         if (center[1]=='center1'){
             center=sf_center(st_geometry(sf))
@@ -37,19 +32,6 @@ zoom<-function(sf,ratio=0.5,center=c(0,0)){
             if (chinese()) stop(paste0(tmcn::toUTF8('sf\u7C7B\u578B\u4E2D\u5FC3\u70B9\u7684geometry\u5FC5\u987B\u662FPOINT')))
             if (!chinese()) stop('center geometry must be point')
         }
-    }
-    if (class(center)=='numeric'){
-        if (length(center)==2){
-            center=matrix(center,ncol = 2)
-        }else{
-            if (chinese()) stop(tmcn::toUTF8('center\u5FC5\u987B2\u4E2A\u6570\u636E,\u5982center=c(0,0)'))
-            if (!chinese()) stop('center must be two number, eg: center=c(0,0)')
-        }
-    }
-    if (nrow(center) != 1 |
-        nrow(center) != nrow(sf)){
-        if (chinese()) stop(tmcn::toUTF8('center\u5FC5\u987B\u662F2\u4E2A\u6570\u5B57,\u5982c(0,0);\u6216\u8005\u548Csf\u7684\u5217\u6570\u76F8\u540C'))
-        if (!chinese()) stop('nrow of center must be two-numeric or equal to nrow of sf')
     }
     res=(st_geometry(sf)-center)*ratio+center
     st_crs(res)=st_crs(sf)
@@ -66,10 +48,6 @@ zoom<-function(sf,ratio=0.5,center=c(0,0)){
 "%z%"<-function(sf,change=list(ratio=0.5,center=c(0,0))){
     ratio=change[[1]]
     center=change[[2]]
-    if (length(ratio) != 1){
-        if (chinese()) stop(tmcn::toUTF8('ratio\u5FC5\u987B\u662F1\u4E2A\u6570\u5B57,\u59820.5'))
-        if (!chinese()) stop('length of ratio must be 1')
-    }
     res=st_geometry(sf)*ratio
     st_crs(res)=st_crs(sf)
     if (any('sf' %in% class(sf))){
